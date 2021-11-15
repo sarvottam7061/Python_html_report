4721d1037c7b8d178abdb71cf022378949daac5a
sarvottam kumar <sarvottamsingh9798@gmail.com>
2021-11-15 19:06:36 +0530

    path changes

--- conftest.py
+++ conftest.py
@@ -1,12 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
 @pytest.fixture
 def selenium_driver():
-    s = Service(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=s)
+    driver = webdriver.Chrome(
+        executable_path="./drivers/chromedriver.exe")
     driver.maximize_window()
     driver.implicitly_wait(2)
     yield driver
--- framework_library/setup_teardown.py
+++ framework_library/setup_teardown.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 def setup():
     driver = webdriver.Chrome(
-        executable_path="/chromedriver.exe")
+        executable_path="C:/Users/843974/PycharmProjects/Python_html_report/drivers/chromedriver.exe")
     driver.maximize_window()
     driver.implicitly_wait(2)
     return driver
