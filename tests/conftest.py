0c816c44497703d601b33208f9f3528a962f668b
sarvottam kumar <sarvottamsingh9798@gmail.com>
2021-11-15 14:31:22 +0530

    path

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.fixture
 def selenium_driver():
     driver = webdriver.Chrome(
-        executable_path="C:/Users/843974/PycharmProjects/Python_html_report/drivers/chromedriver.exe")
+        executable_path="../drivers/chromedriver.exe")
     driver.maximize_window()
     driver.implicitly_wait(2)
     yield driver
--- tests/selenium_easy_test.py
+++ tests/selenium_easy_test.py
@@ -4,11 +4,10 @@ from page_object.AppInputForm import InputForm
 import time
 from reusable_steps.input_form_steps import input_step
 # from framework_library.setup_teardown import setup, teardown
-sys.path.append("../")
+# sys.path.append("../")
 
 def test_me(selenium_driver):
     # selenium_driver = setup()
     input_step(selenium_driver)
     # teardown(selenium_driver)
 
-# test_me()
