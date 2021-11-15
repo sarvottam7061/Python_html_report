c1e5f6bd33a27a5ee83a43a9f33a39d3e1e656aa
sarvottam kumar <sarvottamsingh9798@gmail.com>
2021-11-15 19:11:24 +0530

    path changes

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ import pytest
 @pytest.fixture
 def selenium_driver():
     driver = webdriver.Chrome(
-        executable_path="./drivers/chromedriver.exe")
+        executable_path="chromedriver.exe")
     driver.maximize_window()
     driver.implicitly_wait(2)
     yield driver
