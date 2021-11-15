d48573e60b16e7fc7126a564f36b78f0aa969ff1
sarvottam kumar <sarvottamsingh9798@gmail.com>
2021-11-15 17:01:42 +0530

    path changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 import pytest
-import sys
-sys.path.append("../")
+
 @pytest.fixture
 def selenium_driver():
     driver = webdriver.Chrome(
-        executable_path="../drivers/chromedriver.exe")
+        executable_path="C:/Users/843974/PycharmProjects/Python_html_report/drivers/chromedriver.exe")
     driver.maximize_window()
     driver.implicitly_wait(2)
     yield driver
