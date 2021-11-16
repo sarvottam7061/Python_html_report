a5cdfe669d6ef6c5f249263de8688691c91fb1df
sarvottam kumar <sarvottamsingh9798@gmail.com>
2021-11-16 11:50:18 +0530

    webdriver

--- conftest.py
+++ conftest.py
@@ -1,10 +1,11 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
 @pytest.fixture
 def selenium_driver():
     driver = webdriver.Chrome(
-        executable_path="chromedriver.exe")
+        executable_path=ChromeDriverManager().install())
     driver.maximize_window()
     driver.implicitly_wait(2)
     yield driver
