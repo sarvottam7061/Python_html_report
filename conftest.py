9a38407325aab8e611244c7ab36cea20f5f74e4f
sarvottam kumar <sarvottamsingh9798@gmail.com>
2021-11-16 20:22:52 +0530

    chrome

--- conftest.py
+++ conftest.py
@@ -1,12 +1,18 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 import pytest
 
 @pytest.fixture
 def selenium_driver():
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
     driver = webdriver.Chrome(
-        executable_path=ChromeDriverManager().install())
+        executable_path=ChromeDriverManager().install(), options=chrome_options)
     driver.maximize_window()
     driver.implicitly_wait(2)
     yield driver
     driver.quit()
+
+
